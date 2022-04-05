e2e1d10ea625161a57800614388f0dbeb37fabc5
Conor Hope <c_hope@hotmail.co.uk>
2022-04-05 15:01:05 +0100

    Completed step 8

--- app.rb
+++ app.rb
@@ -4,6 +4,13 @@ require "sinatra/reloader" if development?
 get '/' do
   "hello"      
  end  
+
  get '/secret' do
   "hello again"
+ end
+
+ get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='https://i.imgur.com/jFaSxym.png'>
+  </div>"
  end
